30e60063ff037e49e0d7a093376ad3717f55d932
Albert Dang <98789245+albertdang8@users.noreply.github.com>
2023-05-04 16:57:45 -0700

    footer basically done

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import HomePage from './pages/HomePage/HomePage'
+import Footer from './components/Footer/Footer'
 
 function App () {
   return (
     <div className="App">
       <Header />
       <HomePage />
+      <Footer />
     </div>
   )
 }
---
+++ code/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <div className='footerContainer'>
+        <div className="caption">Made with ❤️ by mimo </div>
+        <a href="" className="contact">Contact Us</a>
+    </div>
+  )
+}
+
+export default Footer
